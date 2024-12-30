029ebb83a00757987e4a07300d185f02ebee7e5e
Oleksandra <165093358+alekssis-u@users.noreply.github.com>
2024-12-29 16:53:03 -0800

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,4 @@
-const container = document.querySelector('.stars-container');
-function createStar() {
-    const existingStar = document.querySelector('.star');
-    if (existingStar) {
-        existingStar.remove();
-    }
-    const star = document.createElement('div');
-    star.classList.add('star');
-    const randomX = Math.random() * window.innerWidth;
-    const randomY = Math.random() * window.innerHeight;
-    star.style.left = `${randomX}px`;
-    star.style.top = `${randomY}px`;
-    container.appendChild(star);
-    star.style.display = 'block';
-    setTimeout(() => {
-        star.style.display = 'none';
-    }, 3000);
-}
-setInterval(createStar, 1000);
-createStar();
+
 const openBtn = document.getElementById("openModal");
 const closeBtn = document.getElementById("closeModal");
 const modal = document.getElementById("modal");
